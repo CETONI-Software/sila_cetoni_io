c8d0f920b4fb67592a8b2378526db718dddc0010
Florian Meinicke <florian.meinicke@cetoni.de>
2022-08-02 15:21:37 +0200

    Update CHANGELOG.md and bump version to v1.5.0

--- sila_cetoni/io/sila/io_service/server.py
+++ sila_cetoni/io/sila/io_service/server.py
@@ -18,7 +18,7 @@ from .generated.analogoutchannelcontroller import AnalogOutChannelControllerFeat
 from .generated.digitalinchannelprovider import DigitalInChannelProviderFeature
 from .generated.digitaloutchannelcontroller import DigitalOutChannelControllerFeature
 
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 
 
 class Server(CoreServer):
